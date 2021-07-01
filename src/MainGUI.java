e7a1bd474932fdea3934faef4ff94498ad550cf2
Arnab Ghosh <arnabcare21@gmail.com>
2021-06-30 20:00:53 -0600

    fixing swing and adding a main function

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,5 +1,25 @@
 import javax.swing.*;
 
-public class MainGUI {
+public class MainGUI extends JFrame{
     private JPanel gui;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    public MainGUI(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(gui);
+        this.pack();
+    }
+    public static void main(String[] args){
+        JFrame window = new MainGUI("Tic Tac Toe");
+        window.setVisible(true);
+
+    }
 }
